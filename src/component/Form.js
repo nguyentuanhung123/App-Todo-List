c98120979bcaa0fa73bf80153011fc40d6bb9a73
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2023-02-23 18:04:12 +0700

    todo-list

---
+++ src/component/Form.js
@@ -0,0 +1,59 @@
+import React, { useEffect } from "react";
+import { v4 as uuidv4 } from "uuid"
+
+const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+    // const onInputChange = (e) => {
+    //     setInput(e.target.value);
+    // }
+
+    const updateTodo = (id, title, completed) => {
+        const newTodo = todos.map((todo) => todo.id === id ? { id, title, completed } : todo);
+        setTodos(newTodo);
+        setEditTodo("");
+    }
+
+    useEffect(() => {
+        if (editTodo) {
+            setInput(editTodo.title)
+        } else {
+            setInput("")
+        }
+    }, [setInput, editTodo])
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        if (!editTodo) {
+            let todo = {
+                id: uuidv4(),
+                title: input,
+                complete: false
+            }
+            setTodos([...todos, todo])
+            setInput("")
+        }
+        else {
+            updateTodo(editTodo.id, input, editTodo.completed)
+        }
+    }
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <input
+                type="text"
+                placeholder="Enter a Todo..."
+                className="task-input"
+                value={input}
+                required
+                onChange={(e) => setInput(e.target.value)}
+            // onChange={(e) => onInputChange()}
+            />
+            <button className="button-add" type="submit">
+                {
+                    editTodo ? "OK" : "Add"
+                }
+            </button>
+        </form>
+    )
+}
+
+export default Form;
